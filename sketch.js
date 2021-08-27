d32797060fa9dc0c156cf377116b6245e32e2786
Gyadav1718 <86192597+Gyadav1718@users.noreply.github.com>
2021-08-27 19:08:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var canvas,backgroundImage;
+var database;
+var form,player,game;
+var playerCount;
+var gamestate = 0;
+function setup(){
+canvas = createCanvas(400,400)
+database = firebase.database();
+
+}
+function draw(){
+  
+}
